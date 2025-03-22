070c0fe075b1cabab7538b53bfb2d2eb55d7fd66
Pavlo Gurskiy <pavlo.gurskiy@gmail.com>
2025-03-22 09:49:19 +0200

    lection 6.1.115

---
+++ codewars.js
@@ -0,0 +1,28 @@
+// function nameToInit (name) {
+//     return name.split(" ").map(word => word[0].toUpperCase()).join('.')
+// }
+// console.log(nameToInit('Pavlo gurskiy'));
+
+// const fruits = ['apple', 'banana'];
+// const string = fruits.join(', ');
+// console.log(string); // apple, banana
+
+// const string = 'apple banana kiwi';
+// const array = string.split(' ');
+// console.log(array); // ['apple', 'banana', 'kiwi']
+
+// const arr = [1,2,3,4,5,6,7,8,9];
+// const even = arr.filter(num => num % 2 === 0);
+// console.log(even); // [2, 4, 6, 8]
+// const first = arr[0]
+// console.log(first);
+
+// function simpleMultiplication(number) {
+//     if (number % 2 === 0) {
+//         return number * 8;
+//     } else {
+//         return number * 9;
+//     }
+// }
+// console.log(simpleMultiplication(2));
+// console.log(simpleMultiplication(3));
